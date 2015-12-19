4c6f584334b11b6d72184c734a2ea30de7e4bb09
YanlongLi <lansunlong@gmail.com>
2015-12-19 16:15:10 +0800

    add Attributepanel.vue

---
+++ src/components/attributePanel.vue
@@ -0,0 +1,23 @@
+<template>
+
+<div>
+	Name: {{curNode.name}}
+	<br>
+	Depth: {{curNode.depth}}
+</div>
+
+</template>
+
+<script lang="coffee">
+
+module.exports=
+	props:
+		curNode: Object
+	data: ()->{}
+
+
+</script>
+
+<style lang="stylus">
+
+</style>
