e677e2ba00ffb9e30fe9cfd591b604f3d41bbeff
YanlongLi <lansunlong@gmail.com>
2015-12-19 16:15:49 +0800

    DataTable component of edit view

---
+++ src/components/DataTable.vue
@@ -0,0 +1,19 @@
+<template>
+
+<table></table>
+
+</template>
+
+<script lang="coffee">
+
+dataStore = require "../utils/DataStore.coffee"
+
+module.exports=
+	data: ()->
+		curDataSet: dataStore || "dataset name"
+
+</script>
+
+<style lang="stylus">
+
+</style>
